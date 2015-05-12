4cac3cc7f0a4b71001e4f691ea8e75d2292d4b1e
Code Salsa <admin@codesalsa.net>
2015-05-12 17:49:07 +0530

    Initial Release

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
 # sliderbase
+
+> A jQuery plugin to integrate a simple jQuery image slider to any website with ease.
+
+## Installing with Bower
+
+To install backTop with Bower:
+
+```bash
+bower install sliderbase
+```
+
+## How to use
+
+- Include ``` <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>``` just before the ```</body>``` tag
+
+- Include ``` <script src="PATH TO YOUR JS DIRECTORY/jquery.sliderbase.js"></script> ``` after the jquery include.
+
+- Include ``` <div id="slider">
+				<ul class="slides">
+					<li class="slide"><img src="img/sliderbase-01.jpg" /></li>
+					<li class="slide"><img src="img/sliderbase-02.jpg" /></li>
+					<li class="slide"><img src="img/sliderbase-03.jpg" /></li>
+					<li class="slide"><img src="img/sliderbase-04.jpeg" /></li>
+					<li class="slide"><img src="img/sliderbase-05.jpeg" /></li>
+				</ul>
+			</div> ``` within your html ``` <body></body> ``` tag
+
+- Include ``` <link href="css/sliderbase.css" rel="stylesheet" type="text/css" /> ``` before the ``` </head> ``` tag.
+
+### Minimum setup
+
+```js
+$(document).ready( function() {
+	$('#slider').sliderBase();
+});
+```
+
+**Example with default options**
+
+```js
+$(document).ready( function() {
+	$('#slider').sliderBase({
+		'interval' : 5000,
+		'speed' : 500,
+		'arrow' : 'true',
+	});
+});
+```
+
+### Interval
+
+To set an interval between each image to animate.
+
+
+### Speed
+
+To specify the slide animation speed between each image.
+
+### Arrow
+
+To specify whether to show or not to show the slider navigation arrows.
+
+
+## Contributing
+
+Please see [CONTRIBUTE.md](CONTRIBUTE.md) for info on contributing.
+
+
+## Demo
+
+<a href="http://codesalsa.net/sliderbase" target="_blank">Check out the demo</a> for more style and feature examples.
+
---
+++ src/jquery.sliderbase.js
@@ -0,0 +1,101 @@
+'use strict';
+
+(function($) {
+
+	$.fn.sliderBase = function(options) {
+	
+		// Slider option Settings
+		var settings = $.extend({
+            'interval' : 5000,
+            'speed' : 500,
+            'arrow' : 'true',
+			'type'	: 'slide',
+        }, options);
+	
+		// Configuration
+		var speed = settings['speed'];
+		var pause = settings['interval'];
+		var arrow = settings['arrow'];
+		var type = settings['slide'];
+		var interval;
+		
+		var $slider = this;
+		var $slideContainer = $slider.find('ul');
+		var $slide = $slideContainer.find('li');
+		var $slideLast = $slideContainer.children('li:last-child');
+		
+		var totalSlides = $slide.length;
+		var slideWidth = $slide.width();
+		var slideHeight = $slide.height();
+		var slideContainerWidth = totalSlides * slideWidth;
+
+		if(arrow =='true'){
+		
+			// Add the Previous and Next Buttons
+			$slider.append("<a id='prev'><span aria-hidden='true' class='glyphicon glyphicon-chevron-left'></span></a><a id='next'><span aria-hidden='true' class='glyphicon glyphicon-chevron-right'></span></a>");
+			
+		}
+		
+		
+		$slider.css({ width: slideWidth, height: slideHeight });
+		
+		$slideContainer.css({ width: slideContainerWidth, marginLeft: - slideWidth });
+		
+		$slideLast.prependTo($slideContainer);
+		
+		var $prevSlide = $('#prev');
+		var $nextSlide = $('#next');
+		
+		
+		function startSlider(){
+			stopLoop();
+			interval = setInterval(function () {
+				moveRight();
+			}, pause);
+			
+			$prevSlide.stop().animate({'left':'-40px'}, {queue: false}, 500).fadeOut();
+			$nextSlide.stop().animate({'right':'-40px'}, {queue: false}, 500).fadeOut();
+		}
+		
+		function pauseSlider(){
+			clearInterval(interval);
+			$prevSlide.fadeIn().animate({'left':'30px'}, {queue: false}, 500);
+			$nextSlide.fadeIn().animate({'right':'30px'}, {queue: false}, 500);
+		}
+		
+		$slider.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
+		
+		
+		
+		function moveLeft() {
+			$slideContainer.animate({
+				left: + slideWidth
+			}, speed, function () {
+				$slideContainer.children('li:last-child').prependTo($slideContainer);
+				$slideContainer.css('left', '');
+			});
+		};
+		
+		function moveRight() {
+			$slideContainer.animate({
+				left: - slideWidth
+			}, speed, function () {
+				$slideContainer.children('li:first-child').appendTo($slideContainer);
+				$slideContainer.css('left', '');
+			});
+		};
+		
+		function stopLoop(){
+			window.clearInterval(interval); //clear interval of loop
+		}
+		
+		$('#prev').click(function () {
+			moveLeft();
+		});
+
+		$('#next').click(function () {
+			moveRight();
+		});
+	}
+	
+}(jQuery));
